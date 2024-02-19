d4a030c5aa9e945d34c6df565356f417cd058a30
Kendry R. Soto <kendryrsoto@gmail.com>
2024-02-19 16:39:34 -0400

    Basic styles and logic

---
+++ src/main.js
@@ -0,0 +1,9 @@
+  const date = document.querySelector('.date');
+  const input = document.querySelector('.input');
+    const button = document.querySelector('.add');
+    const list = document.querySelector('.list');
+    const check = document.querySelector('.check');
+    const remove = document.querySelector('.remove');
+
+    const task = document.querySelector('.task');
+    const taskComplete = document.querySelector('.tasks');
