a68373f285db84bd5e94370b169cf1eb2f3476c4
Kendry R. Soto <kendryrsoto@gmail.com>
2024-02-23 11:32:31 -0400

    adding taskDone function

--- src/main.js
+++ src/main.js
@@ -12,40 +12,39 @@ let id = 0;
 const stylescheckComplete = 'text-decoration-line-through';
 
 function addTask(task, id, done, trash) {
-    if (check.checked) {
-        done = true;
-    }
-    if(trash) {
-        return;
-    }
-  const element = ` <li
+   
+  const element = ` <li 
     class="d-flex align-items-center justify-content-center rounded-pill p-3"
   >
-    <input type="checkbox" id="${id}" />
-    <span id="task" class="mx-5 ${done ? stylescheckComplete : '' }">${task}</span>
-    <button id="${id}" class="btn btn-link ms-2">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="lucide lucide-trash-2"
-      >
-        <path d="M3 6h18" />
-        <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
-        <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
-        <line x1="10" y1="11" x2="10" y2="17" />
-        <line x1="14" y1="11" x2="14" y2="17" />
-      </svg>
-    </button>
+    <input type="checkbox" id="${id}" data='done'/>
+    <span id="task" class="mx-5">${task}</span>
+    <input type="button" id="${id}" class="btn btn-link ms-2" data="remove">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-trash-2"
+              >
+                <path d="M3 6h18" />
+                <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+                <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+                <line x1="10" y1="11" x2="10" y2="17" />
+                <line x1="14" y1="11" x2="14" y2="17" />
+              </svg>
+            </input>
   </li>`;
 
   list.insertAdjacentHTML("beforeend", element);
+}
+
+function taskDone(element) {
+  element.parentNode.querySelector("#task").classList.toggle(stylescheckComplete);
 }
 
 button.addEventListener("click", () => {
@@ -67,9 +66,12 @@ document.addEventListener('keyup', function (e) {
         id++;
     }});
 
-list.addEventListener('click', function (e) {
-    const element = e.target;
-    const elementData = element.attributes.data.value;
-   console.log(elementData);
-   console.log(element);
-});
+    list.addEventListener('click', function (e) {
+        const element = e.target;
+        const elementData = element.attributes.data.value;
+        if (elementData === 'done') {
+            taskDone(element)
+        }else if (elementData === 'remove') {
+            removeTask(element)
+        }
+    });
