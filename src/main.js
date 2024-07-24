41d82b3fb6a37fe9b1230a51a5149d4066530eb4
Kendry R. Soto <kendryrsoto@gmail.com>
2024-07-24 14:17:44 -0400

    Fixing removeTask function

--- src/main.js
+++ src/main.js
@@ -2,12 +2,8 @@ const date = document.querySelector("#date");
 const input = document.querySelector("#input");
 const button = document.querySelector("#add");
 const list = document.querySelector("#list");
-const check = document.querySelector("#check");
-const remove = document.querySelector("#remove");
-const task = document.querySelector(".task");
-const taskComplete = document.querySelector(".tasks");
 let id;
-let LIST;
+let LIST = [];
 
 const stylescheckComplete = "text-decoration-line-through";
 
@@ -19,80 +15,66 @@ date.innerHTML = TM.toLocaleDateString("en-US", {
   year: "numeric",
 });
 
-function addTask(task, id, done, trash) {
-  const element = ` <li 
-    class="d-flex align-items-center justify-content-center rounded-pill p-3"
-  >
-    <input type="checkbox" id="${id}" data='done'/>
-    <span id="task" class="mx-5">${task}</span>
-    <input type="button" id="${id}" class="btn btn-link ms-2" data="remove">
-              <svg
-                data="remove"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="lucide lucide-trash-2"
-              >
-                <path d="M3 6h18" />
-                <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
-                <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
-                <line x1="10" y1="11" x2="10" y2="17" />
-                <line x1="14" y1="11" x2="14" y2="17" />
-              </svg>
-            </input>
-  </li>`;
+function addTask(task, id, done) {
+  const element = `
+    <li class="d-flex align-items-center justify-content-center rounded-pill p-3">
+      <input type="checkbox" class=" ms-2" id="${id}" data='done' ${done ? "checked" : ""}/>
+      <span id="task" class="mx-5 ${
+        done ? stylescheckComplete : ""
+      }">${task}</span>
+      <input type="button" id="${id}" class="btn btn-link ms-2" data="remove" value="delete">
+    </li>`;
 
   list.insertAdjacentHTML("beforeend", element);
 }
 
 function taskDone(element) {
-  element.parentNode
-    .querySelector("#task")
-    .classList.toggle(stylescheckComplete);
-  LIST[element.id].done = LIST[element.id].done ? false : true;
+  const taskSpan = element.parentNode.querySelector("#task");
+  taskSpan.classList.toggle(stylescheckComplete);
+  LIST[element.id].done = !LIST[element.id].done;
+  localStorage.setItem("TDList", JSON.stringify(LIST));
 }
+
 function removeTask(element) {
-  element.parentNode.parentNode.removeChild(element.parentNode);
-  LIST[element.id].trash = true;
+  const taskId = element.id;
+
+  element.parentNode.remove();
+
+  LIST = LIST.filter((task) => task.id != taskId);
+  localStorage.setItem("TDList", JSON.stringify(LIST));
 }
 
 button.addEventListener("click", () => {
-  const task = input.value;
-  if (task) {
-    addTask(task, id, false, false);
+  const taskValue = input.value;
+  if (taskValue) {
+    addTask(taskValue, id, false, false);
     LIST.push({
-      name: task,
+      name: taskValue,
       id: id,
       done: false,
       trash: false,
     });
+    localStorage.setItem("TDList", JSON.stringify(LIST));
+    input.value = "";
+    id++;
   }
-  localStorage.setItem("TDList", JSON.stringify(LIST));
-  input.value = "";
-  id++;
 });
 
 document.addEventListener("keyup", function (e) {
   if (e.key === "Enter") {
-    const task = input.value;
-    if (task) {
-      addTask(task, id, false, false);
+    const taskValue = input.value;
+    if (taskValue) {
+      addTask(taskValue, id, false, false);
       LIST.push({
-        name: task,
+        name: taskValue,
         id: id,
         done: false,
         trash: false,
       });
+      localStorage.setItem("TDList", JSON.stringify(LIST));
+      input.value = "";
+      id++;
     }
-    localStorage.setItem("TDList", JSON.stringify(LIST));
-    input.value = "";
-    id++;
   }
 });
 
@@ -104,7 +86,6 @@ list.addEventListener("click", function (e) {
   } else if (elementData === "remove") {
     removeTask(element);
   }
-  localStorage.setItem("TDList", JSON.stringify(LIST));
 });
 
 let data = localStorage.getItem("TDList");
@@ -119,6 +100,8 @@ if (data) {
 
 function loadList(array) {
   array.forEach(function (item) {
-    addTask(item.name, item.id, item.done, item.trash);
+    if (!item.trash) {
+      addTask(item.name, item.id, item.done, item.trash);
+    }
   });
 }
